9e3df7801b960e01ff746eca166e04485845e7ef
el-shago <92878520+el-shago@users.noreply.github.com>
2023-05-05 09:51:08 -0700

    miau

--- components/seccion2.tsx
+++ components/seccion2.tsx
@@ -1,27 +1,29 @@
-import { HiChatBubbleLeftRight } from "@react-icons/hi";
-
+import { IoChatbubblesOutline } from 'react-icons/io5';
 export function Seccion2() {
   return (
-    <div className="font-Lexend pt-56">
-      <div className="flex flex-col">
+    <div className="font-Lexend pt-52">
+      <div className="flex flex-col justify-center px-20">
         <h2
           className="
-                    pl-36
-                    text-4xl
+                    text-5xl
                     font-bold
                 "
         >
           ¿Por qué usar HandsOn?
         </h2>
-      </div>
-      <div className=" grid grid-cols-3 gap-3 space-x-48 pt-36 border border-black pb-52">
+      
+      <div className=" grid grid-cols-3 gap-3 space-x-48 pt-36 pb-52">
         <div className=" flex flex-col justify-center items-center">
-            <div className="bg-gray-400 rotate-45 rounded-md hover:rotate-[-45] duration-700">pipipupu</div>
-            <div className="absolute bg-primary rounded-md">sexote</div>
+            <div className="bg-gray-400 rotate-45 rounded-3xl hover:rotate-[-45] duration-700 p-16 opacity-50">pipi</div>
+            <div className="absolute bg-primary rounded-3xl">
+              <IoChatbubblesOutline className='h-36 w-36 m-4'/>
+            </div>
 
         </div>
         <div className="bg-blue-500">caca</div>
         <div className="bg-green-500">pis</div>
+        
+      </div>
       </div>
     </div>
   );
