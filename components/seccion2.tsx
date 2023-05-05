52cd70f6fde320636781548d24f11980b87eb53b
el-shago <92878520+el-shago@users.noreply.github.com>
2023-05-04 22:12:17 -0700

    hola mamus y papus

---
+++ components/seccion2.tsx
@@ -0,0 +1,9 @@
+export function Seccion2(){
+    return(
+        <div>
+            hola mamus y papus
+        </div>
+    )
+}
+
+export default Seccion2;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Seccion1 from "../components/seccion1"
+import Seccion2 from "../components/seccion2"
 export default function Home() {
   return (
     <div>
       <Seccion1/>
+      <Seccion2/>
     </div>
     
   )
