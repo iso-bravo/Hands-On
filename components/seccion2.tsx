9d97c1c319f7ec76ed3da63aaa18a837854072e0
el-shago <92878520+el-shago@users.noreply.github.com>
2023-05-06 21:01:39 -0700

    sin grid

--- components/seccion2.tsx
+++ components/seccion2.tsx
@@ -1,5 +1,7 @@
 import { IoChatbubblesOutline } from "react-icons/io5";
 import { TfiWallet } from "react-icons/tfi";
+import { SlScreenSmartphone } from "react-icons/sl";
+
 export function Seccion2() {
   return (
     <div className="font-Lexend py-36">
@@ -13,9 +15,9 @@ export function Seccion2() {
           ¿Por qué usar HandsOn?
         </h2>
 
-        <div className=" grid grid-cols-3 gap-3 space-x-32 px-36 py-24 k">
-          <div className=" flex flex-col justify-center items-center">
-            <div className="bg-gray-400 rotate-45 rounded-3xl p-18 opacity-30 ">
+        <div className="flex flex-row px-36 justify-center py-24 space-x-64">
+          <div className=" flex flex-row justify-center items-center">
+            <div className="bg-gray-400 rotate-45 rounded-3xl p-18 opacity-30">
               chat
             </div>
             <div className="absolute bg-primary rounded-3xl hover:scale-125 transition-all duration-400">
@@ -23,34 +25,31 @@ export function Seccion2() {
             </div>
           </div>
 
-          <div className="grid grid-cols-3 gap-3 space-x-48">
-            <div className="flex flex-col justify-center items-center">
-              <div className="bg-gray-400 rotate-45 rounded-3xl p-18 opacity-30">
-                chat
-              </div>
-              <div className="absolute bg-primary rounded-3xl hover:scale-125 transition-all duration-400">
-                <TfiWallet className="h-36 w-36 m-4" />
-              </div>
+          <div className="flex flex-row justify-center items-center">
+            <div className="bg-gray-400 rotate-45 rounded-3xl p-18 opacity-30">
+              chat
+            </div>
+            <div className="absolute bg-primary rounded-3xl hover:scale-125 transition-all duration-400">
+              <TfiWallet className="h-36 w-36 m-4" />
             </div>
           </div>
-          <div className="grid grid-cols-3 gap-3 space-x-48">
-            <div className="flex flex-col justify-center items-center">
-              <div className="bg-gray-400 rotate-45 rounded-3xl p-18 opacity-30">
-                chat
-              </div>
-              <div className="absolute bg-primary rounded-3xl hover:scale-125 transition-all duration-400">
-                <TfiWallet className="h-36 w-36 m-4" />
-              </div>
-            </div>
-          </div>
-        </div>
 
-        <div className="flex flex-row justify-start px-36 font-bold space-x-48 text-3xl border border-black">
-          <div>Inclusión</div>
-          <div>Gratuito</div>
-          <div>Interactividad</div>
+          <div className="flex flex-col justify-center items-center">
+            <div className="bg-gray-400 rotate-45 rounded-3xl p-18 opacity-30">
+              chat
+            </div>
+            <div className="absolute bg-primary rounded-3xl hover:scale-125 transition-all duration-400">
+              <SlScreenSmartphone className="h-36 w-36 m-4" />
+            </div>
+          </div>
         </div>
       </div>
+
+      <div className="flex flex-col justify-start align-center">
+        <div>Inclusión</div>
+      </div>
+      <div>Gratuito</div>
+      <div>Interactividad</div>
     </div>
   );
 }
